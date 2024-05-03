21592b607d2d01bb1801a7ab35dc712524bf6231
Adrien Unzel <adrien.unzel@neuf.fr>
2024-05-03 10:59:40 +0200

    Chargement image hero aléatoire

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
     $file_name = '/scss/style.css'; // Nom du fichier CSS
@@ -13,9 +13,37 @@ function theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 function register_my_menu() {
-  register_nav_menu( 'main-menu', __( 'Menu principal', 'photographe-event' ) );
+    register_nav_menu( 'main-menu', __( 'Menu principal', 'photographe-event' ) );
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
 
 require_once get_template_directory() . '/menus.php';
+
+
+function getRandomImageURL() {
+  // Obtenir un tableau de toutes les images dans la médiathèque WordPress
+  $args = array(
+      'post_type' => 'attachment',
+      'post_mime_type' => 'image',
+      'posts_per_page' => -1,
+  );
+  $attachments = get_posts($args);
+
+  // Vérifier s'il y a des images disponibles
+  if ($attachments) {
+      // Sélectionner une image aléatoire parmi les images disponibles
+      $random_attachment = $attachments[array_rand($attachments)];
+
+      // Obtenir l'URL de l'image sélectionnée
+      $image_url = wp_get_attachment_url($random_attachment->ID);
+      
+      // Retourner l'URL de l'image sélectionnée
+      return $image_url;
+  } else {
+      // Si aucune image n'est disponible, retourner l'URL de l'image par défaut
+      return get_stylesheet_directory_uri() . '/images/nathalie-11.webp';
+  }
+}
+
+
 ?>
--- home.php
+++ home.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
 
-    <h1>PHOTOGRAPHE EVENT</h1>
+<h1 style="background: url('<?php echo getRandomImageURL(); ?>') lightgray 50% / cover no-repeat;">
+    PHOTOGRAPHE EVENT
+</h1>
 <?php get_footer(); ?>
